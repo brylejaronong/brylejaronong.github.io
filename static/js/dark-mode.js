9be2424250ac5a8e8566c533b3872442802d754e
Bryle Ong <brylejaronong@gmail.com>
2026-07-28 21:00:53 -0700

    chore(dark-mode): fix dark mode reset across nav

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,5 +1,14 @@
+if (localStorage.getItem("theme") === "dark") {
+    document.documentElement.classList.add("dark-mode");
+}
+
 const toggleButton = document.querySelector(".dark-mode-toggle");
 
 toggleButton.addEventListener("click", function() {
     document.documentElement.classList.toggle("dark-mode");
+
+    if (document.documentElement.classList.contains("dark-mode"))
+        localStorage.setItem("theme", "dark");
+    else
+        localStorage.setItem("theme", "light");
 });
